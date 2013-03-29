86706dd95842d33a297f4e44996402fe11c5cb87
mclean.cw <havok2905@gmail.com>
2013-03-28 23:44:40 -0400

    added key controls

--- cypress.js
+++ cypress.js
@@ -9,36 +9,42 @@
 		},
 		build : function()
 		{
-			$("<div class='container' id='image-container'></div>").appendTo("body").hide();
+			
+			$("<div class='container' id='image-container'></div>").appendTo("div#gallery").hide();
 			$("<img src='" + settings.images[settings.position].normal + "' alt='' id='gallery-image'/>").appendTo("div#image-container");
 			$("div#image-container").append("<h2>" + settings.images[settings.position].title + "</h2>");
-			$("div#image-container").append("<a href='prev' id='prev'>prev</a>");
-			$("div#image-container").append("<a href='next' id='next'>next</a>");
+			
+			$("div#image-container").append("<ul id='nav-list'></ul>");
+			$("ul#nav-list").append("<li><a href='prev' id='prev'>prev</a></li>");
+			$("ul#nav-list").append("<li><a href='next' id='next'>next</a></li>");
 			$("div#image-container").append("<a href='next' id='back'>back</a>");
-			$("div#image-container").append("<h3>Downloads</h3>");
-			$("div#image-container").append("<a href='" + settings.images[settings.position].normal + "'>Png</a>");
-			$("div#image-container").append("<a href='" + settings.images[settings.position].normal + "'>Bmp</a>");
-			$("div#image-container").append("<a href='" + settings.images[settings.position].normal + "'>Jpg</a>");
-
+			
+			$("div#gallery").append("<div class='container' id='gallery-container'></div>");
 			$("div#gallery-container").append("<ul></ul>");
 			for(x=0; x<settings.images.length; x++)
 			{
 				$("div#gallery-container ul").append("<li><a href='#' data-role='" + x + "'><img src='" + settings.images[x].thumb + "' alt='" + settings.images[x].desc + "'/></a></li>");
 			}
+			$("div#gallery-container").append("<div class='clear'></div>");
 
-			$("div#gallery-container ul li a").bind("click", function()
+			$("div#gallery-container ul li a").bind("click", function(event)
 			{
-				$("div#gallery-container").fadeOut();
+				event.preventDefault();
 				settings.position = $(this).attr("data-role");
 				$("img#gallery-image").attr("src", settings.images[$(this).attr("data-role")].normal);
-				$("div#image-container").fadeIn();
+				$("div#gallery-container").fadeOut(function()
+				{
+					$("div#image-container").fadeIn();
+				});
 			});
 
 			$("#back").bind("click", function(event)
 			{
 				event.preventDefault();
-				$("div#gallery-container").fadeIn();
-				$("div#image-container").fadeOut();
+				$("div#image-container").fadeOut(function()
+				{
+					$("div#gallery-container").fadeIn();
+				});
 			});
 
 			$("#prev").bind("click", function(event)
@@ -64,6 +70,30 @@
 				$("img#gallery-image").attr("src", settings.images[settings.position].normal);
 				$("#image-container h2").html(settings.images[settings.position].title);
 			});
+
+			$("html").keydown(function(event)
+			{
+				if(event.keyCode == 37)
+				{
+					settings.position--;
+					if(settings.position < 0)
+					{
+						settings.position = settings.images.length - 1;
+					}
+					$("img#gallery-image").attr("src", settings.images[settings.position].normal);
+					$("#image-container h2").html(settings.images[settings.position].title);
+				}
+				else if(event.keyCode == 39)
+				{
+					settings.position++;
+					if(settings.position == settings.images.length)
+					{
+						settings.position = 0;
+					}
+					$("img#gallery-image").attr("src", settings.images[settings.position].normal);
+					$("#image-container h2").html(settings.images[settings.position].title);
+				}			
+			});
 		},
 	};
 
@@ -82,5 +112,4 @@
      		$.error('Method' +  method + 'does not exist on jQuery.cypress');
     	}    
 	};
-
 })(jQuery); 
